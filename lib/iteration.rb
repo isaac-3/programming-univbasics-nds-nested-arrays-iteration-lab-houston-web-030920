e367e406a56bca0158d53db5ea77a186f5b5be6a
isaac chavez <isaacgc0596@gmail.com>
2020-02-01 04:57:48 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -25,9 +25,8 @@ def total_even_pairs(src)
   total = 0
   row_index = 0
   while row_index < src.length do
-    if src[row_index][0] % 2 == 0 && 
-        even_array << num
-      element_index += 1
+    if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
+      total = 
     end
     row_index += 1
   end
