2e3f50d7c88c12669d35cdb30363fe6f4c53615f
isaac chavez <isaacgc0596@gmail.com>
2020-02-01 04:50:19 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,20 +1,41 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  new_array = []
+  row_index = 0
+  while row_index < src.length do
+      new_array << "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
+    row_index += 1
+  end
+  return new_array
 end
 
 def find_greater_pair(src)
+  num_array = []
+  row_index = 0
+  while row_index < src.length do
+    num_array << src[row_index].max
+    row_index += 1
+  end
+num_array
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
 end
 
 def total_even_pairs(src)
+  even_array = []
+  row_index = 0
+  while row_index < src.length do
+    element_index = 0
+    while element_index < src[row_index].count do 
+      src[row_index][element_index].select { |num| num.even? } 
+        even_array << num
+      element_index += 1
+    end
+    row_index += 1
+  end
+  even_array.sum
+  return even_array
+  
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
